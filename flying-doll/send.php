820bc8288cb19f56266d72b072bd9e2517d02f45
Karina <karina_karapetyan@mail.ru>
2020-02-08 06:20:04 -0800

    удалила

--- flying-doll/send.php
+++ flying-doll/send.php
@@ -14,12 +14,12 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP(); 
 $mail->Host = 'smtp.yandex.ua';  
 $mail->SMTPAuth = true;                      
-$mail->Username = 'Kvim.group'; // Ваш логин в Яндексе. Именно логин, без @yandex.ru
-$mail->Password = 'Qwer1212'; // Ваш пароль
+$mail->Username = 'example'; // Ваш логин в Яндексе. Именно логин, без @yandex.ru
+$mail->Password = 'example'; // Ваш пароль
 $mail->SMTPSecure = 'ssl';                            
 $mail->Port = 465;
-$mail->setFrom('Kvim.group@yandex.ua'); // Ваш Email
-$mail->addAddress('Kvim.group@yandex.ua'); // Email получателя
+$mail->setFrom('example'); // Ваш Email
+$mail->addAddress('example'); // Email получателя
 
 // Прикрепление файлов
   for ($ct = 0; $ct < count($_FILES['userfile']['tmp_name']); $ct++) {
