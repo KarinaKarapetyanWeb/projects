44fa0ecd94fb1699d7792dc591d3fe0b548dd559
Karina <karina_karapetyan@mail.ru>
2020-11-04 11:17:33 +0400

    правки

--- project-ru/js/mobile-menu.js
+++ project-ru/js/mobile-menu.js
@@ -1,1 +1,23 @@
-"use strict";!function(){var e=document.querySelector(".main-nav"),a=document.querySelector(".main-nav__toggle"),s=document.querySelector(".page-header__logo");s.classList.remove("page-header__logo--dark"),a.addEventListener("click",function(){e.classList.contains("main-nav--closed")?(e.classList.remove("main-nav--closed"),e.classList.add("main-nav--opened"),s.classList.add("page-header__logo--dark")):(e.classList.add("main-nav--closed"),e.classList.remove("main-nav--opened"),s.classList.remove("page-header__logo--dark"))})}();
+'use strict';
+
+(function () {
+
+	const navMain = document.querySelector('.main-nav');
+	const navToggle = document.querySelector('.main-nav__toggle');
+	const logo = document.querySelector('.page-header__logo');
+
+	logo.classList.remove('page-header__logo--dark');
+
+	navToggle.addEventListener('click', function() {
+		if (navMain.classList.contains ('main-nav--closed')) {
+			navMain.classList.remove('main-nav--closed');
+			navMain.classList.add('main-nav--opened');
+			logo.classList.add('page-header__logo--dark');
+		} else {
+			navMain.classList.add('main-nav--closed');
+			navMain.classList.remove('main-nav--opened');
+			logo.classList.remove('page-header__logo--dark');
+		}
+	});
+
+})();
--- project-ru/js/swiper-init.js
+++ project-ru/js/swiper-init.js
@@ -1,1 +1,91 @@
-"use strict";!function(){if(window.innerWidth<768)new Swiper(".subsection__swiper",{speed:700,spaceBetween:16,slidesPerView:1.15,scrollbar:{el:".swiper-scrollbar"}}),new Swiper(".experts__swiper",{speed:700,spaceBetween:16,slidesPerView:1.15}),new Swiper(".reviews__swiper",{speed:700,spaceBetween:16,slidesPerView:1.15});if(768<=window.innerWidth&&window.innerWidth<1167)new Swiper(".experts__swiper",{speed:700,spaceBetween:16,slidesPerView:2.2}),new Swiper(".reviews__swiper",{speed:700,spaceBetween:16,slidesPerView:2.2});if(1167<=window.innerWidth)new Swiper(".experts__swiper",{speed:700,spaceBetween:20,slidesPerView:3.4,pagination:{el:".swiper-pagination",clickable:!0}}),new Swiper(".reviews__swiper",{speed:700,spaceBetween:20,slidesPerView:3.4,pagination:{el:".swiper-pagination",clickable:!0}})}();
+'use strict';
+
+(function () {
+
+  const DEBOUNCE_INTERVAL = 200;
+  let screenWidth = window.innerWidth;
+
+  const debounce = function (cb) {
+    let lastTimeout = null;
+
+    return (...parameters) => {
+      if (lastTimeout) {
+        window.clearTimeout(lastTimeout);
+      }
+      lastTimeout = window.setTimeout(() => {
+        cb(...parameters);
+      }, DEBOUNCE_INTERVAL);
+    };
+  };
+
+  const updateSLiders = function (width) {
+    if (width < 768) {
+      const swiperLinks = new Swiper('.subsection__swiper', {
+        speed: 700,
+        spaceBetween: 16,
+        slidesPerView: 1.15,
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
+
+      const swiperExpert = new Swiper('.experts__swiper', {
+        speed: 700,
+        spaceBetween: 16,
+        slidesPerView: 1.15,
+      });
+
+      const swiperReview = new Swiper('.reviews__swiper', {
+        speed: 700,
+        spaceBetween: 16,
+        slidesPerView: 1.15,
+      });
+    }   
+
+    if (width >= 768 && window.innerWidth < 1280) {
+      const swiperExpertTablet = new Swiper('.experts__swiper', {
+        speed: 700,
+        spaceBetween: 16,
+        slidesPerView: 2.2,
+      });
+
+      const swiperReviewTablet = new Swiper('.reviews__swiper', {
+        speed: 700,
+        spaceBetween: 16,
+        slidesPerView: 2.2,
+      });
+    }
+
+    if (width >= 1280) {
+      const swiperExpertDesktop = new Swiper('.experts__swiper', {
+        speed: 700,
+        spaceBetween: 20,
+        slidesPerView: 3.4,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+      });
+
+      const swiperReviewDesktop = new Swiper('.reviews__swiper', {
+        speed: 700,
+        spaceBetween: 20,
+        slidesPerView: 3.4,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+      });
+    }
+  }
+
+  const onWindowResize = function () {
+    screenWidth = window.innerWidth;
+    updateSLiders(screenWidth);
+  };
+
+  updateSLiders(screenWidth);
+
+  window.addEventListener('resize', debounce(onWindowResize));
+
+})();
