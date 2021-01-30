4678f8a4f876ad1a76a646049a72ff90c7e73417
Karina <karina_karapetyan@mail.ru>
2021-01-30 17:29:02 +0400

    todolist

---
+++ todolist/precache-manifest.16af48fbcd3dcccf376cbc7fadde6506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4102fefc3500af43a392e9160297b2c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a211d986877765f9ecb",
+    "url": "/static/css/main.3aa072ae.chunk.css"
+  },
+  {
+    "revision": "feb1b647066370ea1297",
+    "url": "/static/js/2.df0683b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.df0683b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a211d986877765f9ecb",
+    "url": "/static/js/main.230e6351.chunk.js"
+  },
+  {
+    "revision": "711714d7d018d95c9ba4",
+    "url": "/static/js/runtime-main.33471999.js"
+  }
+]);
